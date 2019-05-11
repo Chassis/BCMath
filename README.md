aed69aa0faf57861abfd24741f5acd7cfec0ecaa
Bronson Quick <bronson@bronsonquick.com.au>
2019-05-11 21:05:49 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bcmath
+A Chassis extension to install and configure [BCMath](https://www.php.net/manual/en/book.bc.php) on your Chassis server.
+
+## Bcmath
+1. Add this extension to your extensions directory `git clone git@github.com:Chassis/BCMath.git extensions/bcmath`
+2. Set your `config.local.yaml` PHP version to 5.6 or higher.
+3. Run `vagrant provision`.
---
+++ modules/bcmath/manifests/init.pp
@@ -0,0 +1,23 @@
+# A Chassis extension to install and configure Bcmath on your Chassis server
+class bcmath (
+	$config,
+	$path = '/vagrant/extensions/bcmath',
+) {
+	if ( ! empty( $config[disabled_extensions] ) and 'chassis/bcmath' in $config[disabled_extensions] ) {
+		$package = absent
+	} else {
+		$package = latest
+	}
+
+	if versioncmp( $config[php], '5.4') <= 0 {
+		$php_package = 'php5'
+	} else {
+		$php_package = "php${config[php]}"
+	}
+
+	package { "${php_package}-bcmath":
+		ensure  => $package,
+		require => [ Package["${php_package}-cli"], Package["${php_package}-fpm"] ],
+		notify  => Service["${php_package}-fpm"]
+	}
+}
